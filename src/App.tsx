fdc4c86c4c56bd174402eca7aa7f34345df9cfe5
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-05-28 12:16:44 -0500

    actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import MenuItem from "./components/MenuItem"
-import { menuItems } from "./data/db"
-import useOrder from "./hooks/useOrder"
-
+import MenuItem from './components/MenuItem'
+import OrderContents from './components/OrderContents'
+import { menuItems } from './data/db'
+import useOrder from './hooks/useOrder'
 
 function App() {
-
-  const { addItem } = useOrder()
-
+  const { addItem, order } = useOrder()
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black uppercase">Calculadora de propinas y consumo</h1>
+        <h1 className="text-center text-4xl font-black uppercase">
+          Calculadora de propinas y consumo
+        </h1>
       </header>
 
       <main className="max-w-7xl mx-auto py-20  grid md:grid-cols-2">
         <div className="p-5">
           <h2 className="text-4xl font-black">Menu</h2>
           <div className="space-y-3 mt-10">
-            {menuItems.map(item => (
-              <MenuItem
-                key={item.id}
-                item={item}
-                addItem={addItem}
-              />
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
             ))}
           </div>
         </div>
 
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-gap border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents order={order} />
         </div>
       </main>
     </>
--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,23 +1,42 @@
-import { OrderItem } from "../types"
+import { formatCurrency } from '../helpers'
+import { OrderItem } from '../types'
 
 type OrderContentsProps = {
-    order: OrderItem[]
+  order: OrderItem[]
 }
 
 export default function OrderContents({ order }: OrderContentsProps) {
-    return (
-        <div>
-            <h2 className="font-black text-4xl">Consumo</h2>
-            <div className="space-y-3 mt-5">
-                {order.length === 0 ? <p>La orden esta vacia</p>
-                    : (
-                        order.map(item => (
-                            <div key={item.id}>
-                                <p>{item.name} - {item.price}</p>
-                            </div>
-                        ))
-                    )}
+  return (
+    <div>
+      <h2 className="font-black text-4xl">Consumo</h2>
+      <div className="space-y-3 mt-10">
+        {order.length === 0 ? (
+          <p>La orden esta vacia</p>
+        ) : (
+          order.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-between  items-center border-t border-gray-200 py-5 last-of-type:border-b"
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Cantidad: {item.quantity} -{' '}
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button
+                className="bg-red-600 h-8 w-8 text-white font-black rounded-full"
+                type="button"
+              >
+                X
+              </button>
             </div>
-        </div>
-    )
+          ))
+        )}
+      </div>
+    </div>
+  )
 }
